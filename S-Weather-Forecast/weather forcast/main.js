06e6d4ecf6c263602fbcd2f0da8333125ac95fd2
geniusrisky <akashgenius@AKASHs-MacBook-Pro.local>
2022-10-21 17:57:37 +0530

    all done

---
+++ S-Weather-Forecast/weather forcast/main.js
@@ -0,0 +1,29 @@
+//Get All Values
+const inputTxt = document.querySelector('.inputTxt');
+var button = document.querySelector('.btn');
+const showData = document.querySelector('.showData');
+
+//Get API Key By Login To OpenWeather.org, here i am using my api key 
+const API_Key = "6af33e5bba908599e9aa95e223cf1874";
+
+//Now Add Event Listener
+button.addEventListener('click', () => {
+
+    const cityInput = inputTxt.value;
+
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityInput}&units=metric&APPID=${API_Key}`)
+        .then(res => res.json())
+        .then(data => {
+            inputTxt.value = " ";
+            showData.innerHTML = `
+                                <ul>
+                                    <li class="desc">${data.weather[0].description}</li>
+                                    <li class="city">${data.name}</li>
+                                    <li class="temp">${data.main.temp}°c</li>
+                                </ul>
+                                `;
+
+        });
+
+});
+
